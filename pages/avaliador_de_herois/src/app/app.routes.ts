a4a621692d01a7d7355e7559949f7d09b3a9e79a
Diego de Souza Lima <diego.souza@mobilemed.com.br>
2024-08-18 15:28:59 -0300

    tentando subir o deply no vercel com subdiretorio

--- pages/avaliador_de_herois/src/app/app.routes.ts
+++ pages/avaliador_de_herois/src/app/app.routes.ts
@@ -27,5 +27,6 @@ export const routes: Routes = [
         path: 'em-construcao',
         title: 'Em_contrucao',
         component: EmConstrucaoComponent
-    }
+    },
+    { path: '**', redirectTo: '' }
 ];
